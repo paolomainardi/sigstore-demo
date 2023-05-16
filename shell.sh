0626206a8976012e98e967281ca1a0f84610e90e
Paolo Mainardi <paolomainardi@gmail.com>
2023-05-16 15:54:06 +0200

    Add SHELL env variable

--- shell.sh
+++ shell.sh
@@ -4,6 +4,7 @@ export REGISTRY=ghcr.io/paolomainardi/idi2023-sigstore-demo
 export IMAGE=ghcr.io/paolomainardi/idi2023-sigstore-demo
 export DOCKER_TAG=1.0.0
 export COSIGN_EXPERIMENTAL=1
+export SHELL=$(which bash)
 kubectx kind-cosign-demo
 clear
 kubectx kind-cosign-demo
