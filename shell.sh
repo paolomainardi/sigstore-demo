8a161f31de86cccc23be7fc97ad03ddf498dd2bf
Paolo Mainardi <paolomainardi@gmail.com>
2023-06-26 18:49:55 +0200

    drupal demo

--- shell.sh
+++ shell.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
+export OS=$(uname -s | tr '[:upper:]' '[:lower:]')
 export ASCIINEMA_REC=true
 export REGISTRY=ghcr.io/paolomainardi/sigstore-demo
 export IMAGE=ghcr.io/paolomainardi/sigstore-demo
 export DOCKER_TAG=1.0.0
 export COSIGN_EXPERIMENTAL=1
 export SHELL=$(which bash)
+export PATH=$(pwd)/bin/${OS}:$PATH
 kubectx kind-cosign-demo
 clear
 kubectx kind-cosign-demo
