3f7c19b9c2bcd0987ef82962783ff4f952ac426c
Paolo Mainardi <paolomainardi@gmail.com>
2023-10-13 00:37:56 +0200

    feat: add GRYPE_DB_AUTO_UPDATE to false

--- shell.sh
+++ shell.sh
@@ -5,6 +5,7 @@ export IMAGE=ghcr.io/paolomainardi/idi2023-sigstore-demo
 export DOCKER_TAG=1.0.0
 export COSIGN_EXPERIMENTAL=1
 export SHELL=$(which bash)
+export GRYPE_DB_AUTO_UPDATE=false
 kubectx kind-cosign-demo
 clear
 kubectx kind-cosign-demo
